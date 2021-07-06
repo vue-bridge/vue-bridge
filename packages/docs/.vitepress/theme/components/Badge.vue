2cbaf9b5e07353369cc505f12a2e65f185821b85
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-07-06 14:57:11 +0200

    feat(docs): Badge Component

---
+++ packages/docs/.vitepress/theme/components/Badge.vue
@@ -0,0 +1,38 @@
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Badge',
+  props: {
+    type: String, // info, warn, error, success
+    text: String
+  }
+})
+</script>
+<template>
+  <span :class="['badge', type]">
+    <template v-if="text">
+      {{ text }}
+    </template>
+    <slot v-else />
+  </span>
+</template>
+<style lang="postcss" scoped>
+  .badge {
+    display: inline-block;
+    margin: 0 8px;
+    border-radius: 3px;
+    font-size: 0.75em;
+    font-weight: bold;
+    line-height: 0.75em;
+    padding: 4px 4px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #CCC;
+    color: #CCC;
+
+  }
+  .badge.info {
+    border-color: blue;
+    color: blue;
+  }
+</style>
--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -1,9 +1,15 @@
 // import plugin from 'portal-vue'
 import DefaultTheme from 'vitepress/dist/client/theme-default'
-// import VueMonoRepo from 'vue3-compat-lib'
+import { h } from 'vue'
+import Badge from './components/Badge.vue'
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     // app.use(VueMonoRepo)
+    app.component('Badge', Badge)
+    app.component('Info', (props, { slots }) =>
+      h(Badge, { type: 'info', ...props }, slots)
+    )
   },
 }
