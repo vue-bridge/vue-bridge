f7dd50ced558e4d2c5e9819da0f8d2e8181eabfa
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-07-08 20:48:26 +0200

    playground: getting app-vue2 dependencies to properly resolve with vue.demi

--- packages/playground/app-vue2/vite.config.js
+++ packages/playground/app-vue2/vite.config.js
@@ -1,5 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import ts2 from 'rollup-plugin-typescript2'
 const { createVuePlugin } = require('vite-plugin-vue2')
 
 // https://vitejs.dev/config/
@@ -7,7 +8,18 @@ export default defineConfig({
   resolve: {
     alias: {
       vue: path.resolve(__dirname, './node_modules/vue'),
+      'vue3-compat-lib': path.resolve(
+        __dirname,
+        '../../vue3-compat-lib/src/main.ts'
+      ),
+      'example-library': path.resolve(
+        __dirname,
+        '../example-library/src/main.js'
+      ),
     },
   },
-  plugins: [createVuePlugin()],
+  plugins: [createVuePlugin(), ts2()],
+  optimizeDeps: {
+    exclude: ['vue'],
+  },
 })
