b1a8b4ded810595c5677b99b2c740e0835646350
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-07-08 22:45:09 +0200

    feat: eslint-config package

---
+++ packages/eslint-config-vue3-compat/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  rules: {
+    // Template-related
+    'vue/no-multiple-template-root': 'error',
+    'vue/no-v-model-argument': 'error',
+    'vue/no-deprecated-v-bind-sync': 'error',
+    'vue/no-deprecated-v-on-native-modifier': 'error',
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/no-deprecated-filter': 'error',
+
+    // Other
+    'vue/no-deprecated-dollar-listeners-api': 'error',
+    'vue/require-explicit-emits': 'error',
+
+    // enforce alternative exports from `vue3-compat-lib`
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'vue',
+            importNames: ['defineComponent'],
+            message:
+              "Please use `import { defineComponent } from 'vue3-compat-lib'` instead.",
+          },
+        ],
+      },
+    ],
+    'no-restricted-properties': [
+      'error',
+      {
+        property: '$attrs',
+        message: 'Please use $_attrs from attrsListenersMixin instead',
+      },
+      {
+        property: '$listeners',
+        message: 'Please use $_listeners from attrsListenersMixin instead',
+      },
+      {
+        property: '$set',
+        message: 'Please use `set()` exported from `vue3-compat-lib`',
+      },
+      {
+        property: '$delete',
+        message: 'Please use `del()` exported from `vue3-compat-lib`',
+      },
+    ],
+  },
+}
--- packages/playground/example-library/.eslintrc.js
+++ packages/playground/example-library/.eslintrc.js
@@ -1,55 +1,6 @@
 module.exports = {
-  // extends: ['../../../.eslintrc.js'],
+  extends: ['vue3-compat'],
   rules: {
     'unused-imports/no-unused-imports': 0,
-    'vue/no-multiple-template-root': 'error',
-    'vue/require-explicit-emits': 'error',
-    'vue/no-v-model-argument': 'error',
-    'vue/no-deprecated-v-bind-sync': 'error',
-    'vue/no-deprecated-v-on-native-modifier': 'error',
-    'vue/no-use-v-if-with-v-for': 'error',
-    'vue/no-deprecated-dollar-listeners-api': 'error',
-    'vue/no-deprecated-filter': 'error',
-
-    // deactivated as feature not yet implemented / on-hold
-    // 'no-restricted-imports': [
-    //   'error',
-    //   {
-    //     paths: [
-    //       {
-    //         name: 'vue',
-    //         importNames: ['defineAsyncComponent'],
-    //         message:
-    //           "Please use ` import { defineAsyncComponent } from 'vue3-compat-lib/defineAsnyncComponent' instead.",
-    //       },
-    //     ],
-    //   },
-    // ],
-    'no-restricted-imports': [
-      'error',
-      {
-        paths: [
-          {
-            name: 'vue',
-            importNames: ['defineComponent'],
-            message:
-              "Please use ` import { defineComponent } from 'vue3-compat-lib' instead.",
-          },
-        ],
-      },
-    ],
-
-    // recommended in combination with attrsListenersMixin
-    'no-restricted-properties': [
-      'error',
-      {
-        property: '$attrs',
-        message: 'Please use $_attrs from attrsListenersMixin instead',
-      },
-      {
-        property: '$listeners',
-        message: 'Please use $_listeners from attrsListenersMixin instead',
-      },
-    ],
   },
 }
