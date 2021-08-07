024f98b75fae4cde4d0a19c088a567f0cb171017
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-08-07 23:59:56 +0200

    fix(testing): various bugs

--- packages/testing/index.js
+++ packages/testing/index.js
@@ -13,6 +13,7 @@ module.exports = {
 }
 
 function mount(component, options) {
+  // console.log(component)
   if (isVue2) {
     patchProps(options)
     patchGlobals(options)
@@ -45,50 +46,52 @@ function patchGlobals(options) {
 
   const { createLocalVue } = testUtils
   const localVue = createLocalVue()
+  if (options.global) {
+    if (options.global.components) {
+      Object.entries(options.components).forEach(([name, component]) => {
+        localVue.component(name, component)
+      })
+    }
+    if (options.global.directives) {
+      Object.entries(options.directives).forEach(([name, directive]) => {
+        localVue.directive(name, directive)
+      })
+    }
+    if (options.global.plugins) {
+      Object.entries(options.plugins).forEach(([name, plugin]) => {
+        localVue.use(name, plugin)
+      })
+    }
+    if (options.global.mixins) {
+      Object.entries(options.mixins).forEach(([name, mixin]) => {
+        localVue.mixin(name, mixin)
+      })
+    }
 
-  if (options.global.components) {
-    Object.entries(options.components).forEach(([name, component]) => {
-      localVue.component(name, component)
-    })
-  }
-  if (options.global.directives) {
-    Object.entries(options.directives).forEach(([name, directive]) => {
-      localVue.directive(name, directive)
-    })
-  }
-  if (options.global.plugins) {
-    Object.entries(options.plugins).forEach(([name, plugin]) => {
-      localVue.use(name, plugin)
-    })
-  }
-  if (options.global.mixins) {
-    Object.entries(options.mixins).forEach(([name, mixin]) => {
-      localVue.mixin(name, mixin)
-    })
-  }
+    if (options.global.provide) {
+      options.provide = options.global.provide
+    }
+    if (options.global.mocks) {
+      options.mocks = options.global.mocks
+    }
+    if (options.global.stubs) {
+      options.stubs = options.global.stubs
+    }
 
-  if (options.global.provide) {
-    options.provide = options.global.provide
+    delete options.global
   }
-  if (options.global.mocks) {
-    options.mocks = options.global.mocks
-  }
-  if (options.global.stubs) {
-    options.stubs = options.global.stubs
-  }
-
-  delete options.global
 }
 
 function patchUnMount(wrapper) {
   wrapper.unmount = () => wrapper.destroy()
+  return wrapper
 }
 
 function patchSlots(options) {
   if (options.scopedSlots) {
     options.slots = options.slots || {}
+    Object.keys(options.scopedSlots).forEach((key) => {
+      options.slots[key] = options.scopedSlots[key]
+    })
   }
-  Object.keys(options.scopedSlots).forEach((key) => {
-    options.slots[key] = options.scopedSlots[key]
-  })
 }
