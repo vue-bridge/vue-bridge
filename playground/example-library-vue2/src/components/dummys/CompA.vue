e9214a6cf8136a431c2ef8328065b792d2cd8f7f
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-11-28 21:24:18 +0100

    remove copied src files

--- playground/example-library-vue2/src/components/dummys/CompA.vue
+++
@@ -1,9 +0,0 @@
-<script>
-import { defineComponent } from '@vue-bridge/runtime'
-export default defineComponent({
-  name: 'CompA',
-})
-</script>
-<template>
-  <h1>Comp A</h1>
-</template>
--- playground/example-library-vue2/src/components/dummys/CompB.vue
+++
@@ -1,9 +0,0 @@
-<script>
-import { defineComponent } from '@vue-bridge/runtime'
-export default defineComponent({
-  name: 'CompB',
-})
-</script>
-<template>
-  <h1>Comp B</h1>
-</template>
