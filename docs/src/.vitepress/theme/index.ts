53620e51aac4bf7774d3f060632c5285796b7fc8
Thorsten Luenborg <t.luenborg@googlemail.com>
2022-05-14 11:13:40 +0200

    docs: hack page title until we get a theme of our own going.

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -1,9 +1,20 @@
-import { h, App } from 'vue'
+import { h, App, nextTick } from 'vue'
 import { VPTheme } from '@vue/theme'
 import VBBadge from './components/Badge.vue'
 export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
+    nextTick(() => {
+      console.log('tick')
+      const el = document && document.querySelector('.VPNavBarTitle > span')
+      if (el) {
+        el.innerText = 'Vue-Bridge'
+      }
+      const svg = document && document.querySelector('.VPNavBarTitle > svg')
+      if (svg) {
+        svg.remove()
+      }
+    })
     return h(VPTheme.Layout, null, {
       // banner: () => h(Banner),
       // 'sidebar-top': () => h(PreferenceSwitch),
